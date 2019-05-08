14b868b6d4991901d5fa0e81af78b9a8b78b8190
Remdec <remydecocq@gmail.com>
2019-05-08 23:03:40 +0200

    write result fct

--- src/remy/first_try2.R
+++ src/remy/first_try2.R
@@ -22,6 +22,14 @@ test_data_sized <- people[-pred_no_for_train, ]
 
 model <- glm(y ~ ., family = 'binomial', data = train_data)
 
+# Utilities functions
+
+write_csv <- function(pred_probas, filename="outputs/output.csv"){
+  with_ind <- cbind(id=seq.int(length(pred_probas)), prob=pred_probas)
+  probas <- as.data.frame(with_ind)
+  write.csv(probas, file=filename, row.names = FALSE)
+}
+
 show_modinfos <- function(model){
   print(summary(model))
   print(anova(model, test='Chisq'))
@@ -62,8 +70,11 @@ p1 <- hist(test_pred)
 
 print("## using reduced training set to prevent overfitting ###")
 model <- glm(y ~ ., family = 'binomial', data = train_data_sized)
-test_pred <- new_predict(model, obs = test)
-p2 <- hist(test_pred)
+test_pred_sized <- new_predict(model, obs = test)
+p2 <- hist(test_pred_sized)
 
 plot( p1, col=rgb(0,0,1))  # first histogram
 plot( p2, col=rgb(1,0,0, 0.8), add=T)
+
+write_csv(test_pred, filename = "outputs/first_try_nonsized.csv")
+write_csv(test_pred_sized, filename = "outputs/first_try_resized.csv")
